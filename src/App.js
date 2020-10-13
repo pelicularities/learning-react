5c2fa4492aade47ff85104d0ee27e47829aaf1be
Grace Teng <grace@pelicularities.net>
2020-10-13 12:56:17 +0800

    added prettier and prettified src/

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
+    React.createElement("h2", {}, breed),
   ]);
 };
 
@@ -11,9 +11,17 @@ const App = () => {
     "div",
     {},
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" } ),
-    React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" } ),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" } )
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
   );
 };
 
